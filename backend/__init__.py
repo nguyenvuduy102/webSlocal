eb4abcc21cc9bb73144b38061ff78d29702d200c
Vu Duy <vuduy@gmail.com>
2025-12-31 10:41:41 +0700

    sua forget pass

--- backend/__init__.py
+++ backend/__init__.py
@@ -31,8 +31,12 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 app.config["PAGE_SIZE"] = 12
 #mail
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+
+
+app.config['MAIL_PORT'] = 465                 # Đổi sang cổng 465
+app.config['MAIL_USE_TLS'] = False            # Tắt TLS
+app.config['MAIL_USE_SSL'] = True
+
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') # Dùng biến môi trường cho bảo mật
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') # Mật khẩu ứng dụng
 
